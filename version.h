041e4f5b702922350f604ad77ced2293cc100e1d
Ondřej Hruška <ondra@ondrovo.com>
2018-04-16 09:06:22 +0200

    version increment

--- version.h
+++ version.h
@@ -7,6 +7,16 @@
 #ifndef GEX_VERSION_H
 #define GEX_VERSION_H
 
-#define GEX_VERSION "0.2.0"
+#define GEX_VERSION "0.3.0"
+
+// --- 0.3.0 ---
+// + Possible fallback to UART PA2,PA3 if USB not available
+// + Possible fallback to NRF24L01+
+
+// --- 0.2.0 ---
+// + ADC constant reading
+
+// --- 0.1.0 ---
+// First public test build
 
 #endif //GEX_VERSION_H
