30d1762710170087901981b140ee84719628fda1
Ondřej Hruška <ondra@ondrovo.com>
2018-04-23 16:11:46 +0200

    version

--- version.h
+++ version.h
@@ -7,11 +7,16 @@
 #ifndef GEX_VERSION_H
 #define GEX_VERSION_H
 
-#define GEX_VERSION "0.3.0"
+#define GEX_VERSION "0.3.1"
+
+// --- 0.3.1 ---
+// * Fix ADC triggers
 
 // --- 0.3.0 ---
 // + Possible fallback to UART PA2,PA3 if USB not available
 // + Possible fallback to NRF24L01+
+// * Partial fix for ADC EOS handling
+// + Option to disable ADC averaging
 
 // --- 0.2.0 ---
 // + ADC constant reading
